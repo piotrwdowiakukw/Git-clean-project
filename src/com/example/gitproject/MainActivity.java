33bbb357290025d61f3996b58fb55f760bbf5cc7
Piotr Wdowiak <piotrwdowiak@op.pl>
2014-02-03 21:41:05 +0100

    Add a flash button

--- src/com/example/gitproject/MainActivity.java
+++ src/com/example/gitproject/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.gitproject;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	Button b;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		b = (Button) findViewById(R.id.button1);
+		b.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "B£YSK!", 1).show();
+			}
+		});
 	}
 
 	@Override
